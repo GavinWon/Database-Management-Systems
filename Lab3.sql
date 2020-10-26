606de126c27c8186d37e5777e7cdcf47580ed0a6
Gavin <gavinmwong@gmail.com>
2020-10-26 15:26:53 -0700

    finished lab 3

--- Lab3.sql
+++ Lab3.sql
@@ -27,6 +27,21 @@ Select Distinct ID
 from takes
 Where takes.course_ID LIKE 'CS%'
 
+Select instructor.ID
+from teaches
+RIGHT JOIN instructor
+ON instructor.ID = teaches.ID
+Where course_id IS NULL
 
+Select instructor.name, instructor.ID
+from teaches
+RIGHT JOIN instructor
+ON instructor.ID = teaches.ID
+Where course_id IS NULL
+
+Create VIEW CSInstructors AS
+Select ID, name, dept_name
+from instructor
+Where dept_name = 'Comp. Sci.';
 
 
